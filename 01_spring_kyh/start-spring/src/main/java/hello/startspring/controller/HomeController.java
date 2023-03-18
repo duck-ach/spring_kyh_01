84bef2505d941529c0b1203ac7c66950cf669cc5
duck_ach <djagmlfk@naver.com>
2023-03-18 16:09:35 +0900

    회원등록 이지하군

--- 01_spring_kyh/start-spring/src/main/java/hello/startspring/controller/HomeController.java
+++ 01_spring_kyh/start-spring/src/main/java/hello/startspring/controller/HomeController.java
@@ -1,14 +1,12 @@
 package hello.startspring.controller;
 
+import hello.startspring.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
 
 }
--- 01_spring_kyh/start-spring/src/main/java/hello/startspring/controller/MemberController.java
+++ 01_spring_kyh/start-spring/src/main/java/hello/startspring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.startspring.controller;
 
+import hello.startspring.domain.Member;
 import hello.startspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -24,4 +27,26 @@ public class MemberController {
 //    public void setMemberService(MemberService memberService) {
 //        this.memberService = memberService;
 //    }
+
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+
 }
