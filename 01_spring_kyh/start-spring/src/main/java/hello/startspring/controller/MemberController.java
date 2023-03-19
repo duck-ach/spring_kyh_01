39e83d0f623b2a0c77e0c9cf2e5542555819cbf4
duck_ach <djagmlfk@naver.com>
2023-03-19 21:09:48 +0900

    list

--- 01_spring_kyh/start-spring/src/main/java/hello/startspring/controller/MemberController.java
+++ 01_spring_kyh/start-spring/src/main/java/hello/startspring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.startspring.domain.Member;
 import hello.startspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -48,5 +51,11 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 
 }
