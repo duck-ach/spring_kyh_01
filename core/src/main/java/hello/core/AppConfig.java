9010ff1cdd7ae8552a562f425c8894a5fe5acecf
duck_ach <djagmlfk@naver.com>
2023-04-19 19:10:11 +0900

    Spring Bean

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -7,25 +7,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
  * AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다.
  */
+@Configuration
 public class AppConfig { /* command + option + m 을 사용하여 AppConfig Refactoring */
 
+    @Bean
     public MemberService memberService() { // MemoryMemberRepository를 Autowired 한것과 같음 의존성 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
