168045363d279f21c162d5edcc533c6129518528
duck_ach <djagmlfk@naver.com>
2023-05-03 22:41:51 +0900

    ComponentScan

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 // 인터페이스의 구현체가 하나만 있을 경우 관례적으로 이름에 Impl을 많이 붙인다.
+@Component("memberService2")
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    // ac.getBean(memberRepository.class) == @Autowired
+    @Autowired // 의존관계를 자동으로 주입해주는 기능 Component를 사용하면 @Autowired를 쓰게 된다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
