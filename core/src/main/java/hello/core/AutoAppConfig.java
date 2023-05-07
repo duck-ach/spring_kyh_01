d278eff9f37f3dba1357cdeb712abe1397142cf0
duck_ach <djagmlfk@naver.com>
2023-05-07 17:45:01 +0900

    Configuration Scan, base pakages 설정

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,18 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan (// 스프링 빈을 자동으로 끌어올려주는 애너테이션
+
+        // basePackages를 설정해주면 해당 경로부터 하위경로의 Bean 들을 찾는다.
+        // 라이브러리같은 것들을 사용할 때 많이 사용한다.
+        // basePackages = "hello.core.member",
+
+        // 해당 클래스가 포함된 패키지의 경로(package hello.core)부터 찾는다.
+        // basePackageClasses = AutoAppConfig.class,
+
+        // 만약 위의 base들을 설정하지 않으면 @ComponentScan이 붙은 설정 정보의 클래스의 패키지가 시작 위치가 된다.
+
         // AppConfig가 자동으로 등록되지 않도록 (충돌방지)
-    excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 /**
  * 컴포넌트 스캔을 사용하면 @Configuration 이 붙은 설정 정보도 자동으로 등록되기 때문에, AppConfig, TestConfig 등 앞서 만들어 두었던
