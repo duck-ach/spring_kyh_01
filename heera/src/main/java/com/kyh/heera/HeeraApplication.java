a043c803591537cb507937034b90a7ec2778e945
duck_ach <djagmlfk@naver.com>
2024-02-15 22:48:02 +0900

    create project

---
+++ heera/src/main/java/com/kyh/heera/HeeraApplication.java
@@ -0,0 +1,12 @@
+package com.kyh.heera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeeraApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HeeraApplication.class, args);
+	}
+
+}
