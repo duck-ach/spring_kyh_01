6aa9ceaeb87c24bc0b7585c2857fcf9037ef0be0
duck_ach <djagmlfk@naver.com>
2024-02-20 00:06:26 +0900

    OrderService 구현

---
+++ heera/src/main/java/com/kyh/heera/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package com.kyh.heera.order;
+
+import com.kyh.heera.discount.DiscountPolicy;
+import com.kyh.heera.discount.FixDiscountPolicy;
+import com.kyh.heera.member.Member;
+import com.kyh.heera.member.MemberRepository;
+import com.kyh.heera.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 회원정보 조회
+        Member member = memberRepository.findById(memberId);
+
+        // 할인정책에 회원 넘기기
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        // 주문에 회원정보와 할인정보 넘기기
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
