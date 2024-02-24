9bc931db2f483368872d288f49399333ab6724e7
duck_ach <djagmlfk@naver.com>
2024-02-24 21:38:12 +0900

    rate order 개발

---
+++ heera/src/main/java/com/kyh/heera/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.kyh.heera.discount;
+
+import com.kyh.heera.member.Grade;
+import com.kyh.heera.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
