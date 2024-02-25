78d43000910ffc308b75456953ca5821104fbec1
duck_ach <djagmlfk@naver.com>
2024-02-25 16:22:03 +0900

    [Re-Factoring] AppConfig 중복 제거 및 역할분리

--- heera/src/main/java/com/kyh/heera/AppConfig.java
+++ heera/src/main/java/com/kyh/heera/AppConfig.java
@@ -1,6 +1,8 @@
 package com.kyh.heera;
 
+import com.kyh.heera.discount.DiscountPolicy;
 import com.kyh.heera.discount.FixDiscountPolicy;
+import com.kyh.heera.member.MemberRepository;
 import com.kyh.heera.member.MemberService;
 import com.kyh.heera.member.MemberServiceImpl;
 import com.kyh.heera.member.MemoryMemberRepository;
@@ -14,13 +16,25 @@ public class AppConfig {
      * 객체를 생성하고 연결하는 책임을 가지는 별도의 설정 클래스
      */
 
+    /**
+     * [Refactoring]
+     * 아래와 같이 AppConfig를 구성하면
+     * Method 를 호출하는 순간 역할이 다 드러난다.
+     */
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 }
