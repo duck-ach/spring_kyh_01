4c7db316657d4810b90abdfbed8e62ec7dffcf75
duck_ach <djagmlfk@naver.com>
2024-03-11 23:01:01 +0900

    provider를 이용하여 prototype bean 제작

--- heera/src/test/java/com/kyh/heera/scope/SingletonWithPrototypeTest1.java
+++ heera/src/test/java/com/kyh/heera/scope/SingletonWithPrototypeTest1.java
@@ -4,9 +4,13 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -24,8 +28,9 @@ public class SingletonWithPrototypeTest1 {
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
      }
 
-     @Test
-     void singletonClientUsePrototype() {
+
+    @Test
+    void singletonClientUsePrototype() {
          AnnotationConfigApplicationContext ac =
                  new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
@@ -35,27 +40,35 @@ public class SingletonWithPrototypeTest1 {
 
          ClientBean clientBean2 = ac.getBean(ClientBean.class);
          int count2 = clientBean2.logic();
-         assertThat(count2).isEqualTo(2);
+         assertThat(count2).isEqualTo(1);
 
          System.out.println("count1 = " + count1);
          System.out.println("count2 = " + count2);
 
-     }
+    }
 
-     @Scope("singleton")
-     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+    @Scope("singleton")
+    @Component
+    static class ClientBean {
 
-         public ClientBean(PrototypeBean prototypeBean) {
-             this.prototypeBean = prototypeBean;
-         }
+        // ObjectProvider
+        @Autowired
+        private PrototypeBean prototypeBean; // 생성 시점에 주입
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
-         public int logic() {
+//        // JSR330 Provider
+//        // implementation 'javax.inject:javax.inject:1' gradle 추가 필수
+//        @Autowired
+//        private Provider<PrototypeBean> provider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
-         }
-     }
+        }
+    }
 
     /**
      *  여러 Bean 에서 같은 prototypeBean 을 주입 받으면,
@@ -65,22 +78,9 @@ public class SingletonWithPrototypeTest1 {
      *  - clientB -> prototypeBean@x02
      *  물론, 사용할 때마다 새로 생성되는 것은 아니다.
      */
-    @Scope("singleton")
-     static class ClientBean2 {
-        private final PrototypeBean prototypeBean; // ClientBean의 prototypeBean과 다른 Bean
-
-         public ClientBean2(PrototypeBean prototypeBean) {
-             this.prototypeBean = prototypeBean;
-         }
-
-         public int logic() {
-            prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
-         }
-     }
 
     @Scope("prototype")
+    @Component
     static class PrototypeBean {
         private int count = 0;
 
