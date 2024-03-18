2440e9274ad3f11c832cac8bc64efd57093d31cc
duck_ach <djagmlfk@naver.com>
2024-03-18 21:01:41 +0900

    hello servlet 및 index페이지

--- mvc/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ mvc/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 하위 패키지를 다 뒤져서 서블릿을 다 실행할 수 있도록 해준다.
 @SpringBootApplication
 public class ServletApplication {
 
