89664784414d4889a25c8814a9e26dd0ad38d860
duck_ach <djagmlfk@naver.com>
2024-03-26 22:27:58 +0900

    Member Repository 생성 및 포함 테스트 작성

---
+++ mvc/servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,46 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+public class MemberRepository {
+
+    private Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // store의 모든값을 꺼내어서 새로운 ArrayList에 넣어준다. (store에 있는 값들을 건들지 않기 위해)
+    }
+
+    /**
+     * Test 같은 곳에서만 사용하는 것
+     */
+    public void clearStore() {
+        store.clear();
+    }
+}
