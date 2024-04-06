9ac4a98c7e2bbf0498eba2a519e4b946b2360de3
duck_ach <djagmlfk@naver.com>
2024-04-06 15:51:13 +0900

    controller dispatcher 중복 제거

---
+++ mvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,49 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.contoller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.contoller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.contoller.MemberSaveControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// /front-controller/v1/* 하위로 들어오는 호출은 모두 이 컨트롤러로 들어옴
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV2.service");
+        String requestURI = request.getRequestURI();
+
+        // Controller에서 찾기
+        ControllerV2 controller = controllerMap.get(requestURI);
+
+        if(controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 잘 조회가 됐다 == controller 에 request, response 담아서 호출
+        controller.process(request, response);
+
+        MyView view = controller.process(request, response);
+        view.render(request, response);
+
+    }
+}
